e1dc1113bbe1b39abf089c4b87d2dce5e60da521
Ethan Rowe <ethanrowe5379@gmail.com>
2024-11-11 16:04:25 +0000

    Since we have the Hash function available, ive implemented the insert functions for the table.

--- HashTable.c
+++ HashTable.c
@@ -66,3 +66,18 @@ static int ht_get_hash(const char* s, const int num_buckets, const int attempts)
 }
 
 /*Add Item to Hash Table*/
+void ht_insert(Hash_Table* ht, const char* key, const char* value){
+    ht_item* item = ht_new_item(key, value);
+    
+    int index = ht_get_hash(item->key, ht->size, 0);
+    ht_item* cur_item = ht->items[index];
+    
+    int i = 1;
+    while(cur_item != NULL){
+        index = ht_get_hash(item->key, ht->size, i);
+        cur_item = ht->items[index];
+        i++;  
+    }
+    ht->items[index] = item;
+    ht->count++;
+}
